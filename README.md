b5ef964438fdfe81ed22308fb2a49194bab52f20
yuki <mimi13149420@gmail.com>
2024-09-21 15:16:45 +0800

    前端基礎檢設

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# HealthyDiet 123
+# HealthyDiet 
+
+
+### 前端開發
+* Layout - index.html 與 main.js  
+透過main.js去實作切換頁面，請在元素上掛 data-content，並且把對應的html放在view資料夾底下
+
---
+++ frontend/src/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    // --------處理換頁 START----------
+    // 導覽列點擊事件
+    $('.nav-item').on('click', function(e) {
+        e.preventDefault();
+        const content = $(this).data('content');
+        loadContent(content);
+    });
+
+    // 功能區點擊事件
+    $('.list-group-item').on('click', function(e) {
+        e.preventDefault();
+        const feature = $(this).data('content');
+        loadContent(feature);
+    });
+    
+    // 加載內容
+    function loadContent(file) {
+        file = "src/view/" + file;
+        $('#content-area').load(file, function(response, status, xhr) {
+            if (status == "error") {
+                const msg = "出現錯誤: " + xhr.status + " " + xhr.statusText;
+                $('#content-area').html(msg);
+            }
+        });
+    }
+
+    loadContent("home.html")
+
+    // --------處理換頁 END----------
+
+    // --------處理sidebar問題 START----------
+    function toggleSidebar() {
+        $("#sidebar").toggleClass("active");
+        $("#content-area").toggleClass("active");
+    }
+
+    $("#sidebarToggle").click(function(e) {
+        e.preventDefault();
+        toggleSidebar();
+    });
+
+    $("#closeSidebar").click(function(e) {
+        e.preventDefault();
+        toggleSidebar();
+    });
+    // --------處理sidebar問題 END----------
+
+});
